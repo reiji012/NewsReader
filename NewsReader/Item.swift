8e867df4f94d9b33ee40c8e7d9bc20b29f136348
Reiji <reiji0120124@gmail.com>
2018-11-19 21:57:40 +0900

    first commit

---
+++ NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by 松村礼二 on 2018/11/17.
+//  Copyright © 2018年 松村礼二. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var link = ""
+}
